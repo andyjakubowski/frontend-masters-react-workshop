caf10a07b9f19de14db9cd84fbeeabbe6f5eeee3
Andy Jakubowski <hello@andyjakubowski.com>
2021-01-21 16:44:54 +0100

    Finish exercise 04

--- src/04/timerMachine.js
+++ src/04/timerMachine.js
@@ -1,49 +1,64 @@
 import { createMachine, assign } from 'xstate';
 
-export const timerMachine = createMachine({
-  initial: 'idle',
-  context: {
-    duration: 5,
-    elapsed: 0,
-    interval: 0.1,
-  },
-  states: {
-    idle: {
-      entry: assign({
-        duration: 5,
-        elapsed: 0,
-      }),
-      on: {
-        TOGGLE: 'running',
-      },
+export const timerMachine = createMachine(
+  {
+    initial: 'idle',
+    context: {
+      duration: 5,
+      elapsed: 0,
+      interval: 0.1,
     },
-    running: {
-      on: {
-        // Change this TICK transition into a guarded transition
-        // to go to `expired` when `context.elapsed + context.interval`
-        // is greater than the total `context.duration`.
-        TICK: {
-          actions: assign({
-            elapsed: (ctx) => ctx.elapsed + ctx.interval,
-          }),
-        },
-        TOGGLE: 'paused',
-        ADD_MINUTE: {
-          actions: assign({
-            duration: (ctx) => ctx.duration + 60,
-          }),
+    states: {
+      idle: {
+        entry: assign({
+          duration: 5,
+          elapsed: 0,
+        }),
+        on: {
+          TOGGLE: 'running',
         },
       },
-    },
-    paused: {
-      on: {
-        TOGGLE: 'running',
-        RESET: 'idle',
+      running: {
+        on: {
+          TICK: [
+            {
+              cond: 'doesNotExceedDuration',
+              actions: assign({
+                elapsed: (ctx) => ctx.elapsed + ctx.interval,
+              }),
+            },
+            {
+              target: 'expired',
+            },
+          ],
+          TOGGLE: 'paused',
+          ADD_MINUTE: {
+            actions: assign({
+              duration: (ctx) => ctx.duration + 60,
+            }),
+          },
+        },
+      },
+      paused: {
+        on: {
+          TOGGLE: 'running',
+          RESET: 'idle',
+        },
+      },
+      expired: {
+        on: {
+          RESET: {
+            target: 'idle',
+          },
+        },
       },
     },
-
-    // Add an `expired` state here.
-    // It should go to the `idle` state on the `RESET` event.
-    // ...
   },
-});
+  {
+    guards: {
+      doesNotExceedDuration: (ctx) => {
+        return ctx.elapsed + ctx.interval <= ctx.duration;
+      },
+    },
+  }
+);
